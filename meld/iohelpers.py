00364a94ff0fe9ce97c8eb522c059b3ec36fce7c
Christoph Brill <opensource@christophbrill.de>
2025-04-14 13:16:49 +0200

    Derive copyright headers from git log
    git log --follow --format="%an <%ae>" FILENAME | sort -u
    followed by
    git log --follow --reverse --format="%ad" --date=short FILENAME | grep EMAIL

--- meld/iohelpers.py
+++ meld/iohelpers.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2018-2024 Kai Willadsen <kai.willadsen@gmail.com>
+# Copyright (C) 2023 luzpaz <kunda@scribus.net>
+# Copyright (C) 2020 Mario Aichinger <aichingm@gmail.com>
+# Copyright (C) 2025 Christoph Brill <opensource@christophbrill.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import pathlib
 from typing import Optional, Sequence
