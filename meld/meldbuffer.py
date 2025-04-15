65e89df4705d316d03cdf8613e0963d6ef2c5aff
Christoph Brill <opensource@christophbrill.de>
2025-04-15 17:22:12 +0200

    Add typings to meldbuffer.py

--- meld/meldbuffer.py
+++ meld/meldbuffer.py
@@ -18,10 +18,11 @@ import enum
 import logging
 from typing import Any, List, Optional
 
-from gi.repository import Gio, GLib, GObject, GtkSource
+from gi.repository import Gio, GLib, GObject, Gtk, GtkSource
 
 from meld.conf import _
 from meld.settings import bind_settings
+from meld.undo import UndoSequence
 
 log = logging.getLogger(__name__)
 
@@ -34,21 +35,21 @@ class MeldBuffer(GtkSource.Buffer):
         ('highlight-syntax', 'highlight-syntax'),
     )
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         bind_settings(self)
-        self.data = MeldBufferData()
-        self.undo_sequence = None
+        self.data: MeldBufferData = MeldBufferData()
+        self.undo_sequence: Optional[UndoSequence] = None
 
-    def do_begin_user_action(self, *args):
+    def do_begin_user_action(self, *args: Any) -> None:
         if self.undo_sequence:
             self.undo_sequence.begin_group()
 
-    def do_end_user_action(self, *args):
+    def do_end_user_action(self, *args: Any) -> None:
         if self.undo_sequence:
             self.undo_sequence.end_group()
 
-    def get_iter_at_line_or_eof(self, line):
+    def get_iter_at_line_or_eof(self, line: int) -> Gtk.TextIter:
         """Return a Gtk.TextIter at the given line, or the end of the buffer.
 
         This method is like get_iter_at_line, but if asked for a position past
@@ -60,7 +61,7 @@ class MeldBuffer(GtkSource.Buffer):
             return self.get_end_iter()
         return self.get_iter_at_line(line)
 
-    def insert_at_line(self, line, text):
+    def insert_at_line(self, line: int, text: str) -> Gtk.TextIter:
         """Insert text at the given line, or the end of the buffer.
 
         This method is like insert, but if asked to insert something past the
@@ -87,6 +88,13 @@ class MeldBufferState(enum.Enum):
 class MeldBufferData(GObject.GObject):
 
     state: MeldBufferState
+    _gfile: Optional[Gio.File]
+    _label: Optional[str]
+    _monitor: Optional[Any]
+    _sourcefile: Optional[GtkSource.File]
+    savefile: Optional[Gio.File]
+    _disk_mtime: Optional[tuple[int, int]]
+    _mtime: Optional[tuple[int, int]]
 
     @GObject.Signal('file-changed')
     def file_changed_signal(self) -> None:
@@ -98,15 +106,18 @@ class MeldBufferData(GObject.GObject):
         default=GtkSource.Encoding.get_utf8(),
     )
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self._gfile = None
-        self._label = None
-        self._monitor = None
-        self._sourcefile = None
+        self._gfile: Optional[Gio.File] = None
+        self._label: Optional[str] = None
+        self._monitor: Optional[Any] = None
+        self._sourcefile: Optional[GtkSource.File] = None
+        self.savefile: Optional[Gio.File] = None
+        self._disk_mtime: Optional[tuple[int, int]] = None
+        self._mtime: Optional[tuple[int, int]] = None
         self.reset(gfile=None, state=MeldBufferState.EMPTY)
 
-    def reset(self, gfile: Optional[Gio.File], state: MeldBufferState):
+    def reset(self, gfile: Optional[Gio.File], state: MeldBufferState) -> None:
         same_file = gfile and self._gfile and gfile.equal(self._gfile)
         self.gfile = gfile
         if same_file:
@@ -116,16 +127,16 @@ class MeldBufferData(GObject.GObject):
         self.state = state
         self.savefile = None
 
-    def __del__(self):
+    def __del__(self) -> None:
         self.disconnect_monitor()
 
     @property
-    def label(self):
+    def label(self) -> str:
         # TRANSLATORS: This is the label of a new, currently-unnamed file.
         return self._label or _("<unnamed>")
 
     @label.setter
-    def label(self, value):
+    def label(self, value: Optional[str]) -> None:
         if not value:
             return
         if not isinstance(value, str):
@@ -133,14 +144,14 @@ class MeldBufferData(GObject.GObject):
             return
         self._label = value
 
-    def connect_monitor(self):
+    def connect_monitor(self) -> None:
         if not self._gfile:
             return
         monitor = self._gfile.monitor_file(Gio.FileMonitorFlags.NONE, None)
         handler_id = monitor.connect('changed', self._handle_file_change)
         self._monitor = monitor, handler_id
 
-    def disconnect_monitor(self):
+    def disconnect_monitor(self) -> None:
         if not self._monitor:
             return
         monitor, handler_id = self._monitor
@@ -148,7 +159,7 @@ class MeldBufferData(GObject.GObject):
         monitor.cancel()
         self._monitor = None
 
-    def _query_mtime(self, gfile):
+    def _query_mtime(self, gfile: Gio.File) -> Optional[tuple[int, int]]:
         try:
             time_query = ",".join((Gio.FILE_ATTRIBUTE_TIME_MODIFIED,
                                    Gio.FILE_ATTRIBUTE_TIME_MODIFIED_USEC))
@@ -158,18 +169,18 @@ class MeldBufferData(GObject.GObject):
         mtime = info.get_modification_time()
         return (mtime.tv_sec, mtime.tv_usec)
 
-    def _handle_file_change(self, monitor, f, other_file, event_type):
+    def _handle_file_change(self, monitor: Gio.FileMonitor, f: Gio.File, other_file: Optional[Gio.File], event_type: int) -> None:
         mtime = self._query_mtime(f)
         if self._disk_mtime and mtime and mtime > self._disk_mtime:
             self.file_changed_signal.emit()
         self._disk_mtime = mtime or self._disk_mtime
 
     @property
-    def gfile(self):
+    def gfile(self) -> Optional[Gio.File]:
         return self._gfile
 
     @gfile.setter
-    def gfile(self, value):
+    def gfile(self, value: Optional[Gio.File]) -> None:
         self.disconnect_monitor()
         self._gfile = value
         self._sourcefile = GtkSource.File()
@@ -181,15 +192,15 @@ class MeldBufferData(GObject.GObject):
         self.connect_monitor()
 
     @property
-    def sourcefile(self):
+    def sourcefile(self) -> Optional[GtkSource.File]:
         return self._sourcefile
 
     @property
-    def gfiletarget(self):
+    def gfiletarget(self) -> Optional[Gio.File]:
         return self.savefile or self.gfile
 
     @property
-    def is_special(self):
+    def is_special(self) -> bool:
         try:
             info = self._gfile.query_info(
                 Gio.FILE_ATTRIBUTE_STANDARD_TYPE, 0, None)
@@ -206,7 +217,7 @@ class MeldBufferData(GObject.GObject):
             return None
 
     @property
-    def writable(self):
+    def writable(self) -> bool:
         try:
             info = self.gfiletarget.query_info(
                 Gio.FILE_ATTRIBUTE_ACCESS_CAN_WRITE, 0, None)
@@ -218,12 +229,12 @@ class MeldBufferData(GObject.GObject):
             return False
         return info.get_attribute_boolean(Gio.FILE_ATTRIBUTE_ACCESS_CAN_WRITE)
 
-    def update_mtime(self):
+    def update_mtime(self) -> None:
         if self._gfile:
             self._disk_mtime = self._query_mtime(self._gfile)
             self._mtime = self._disk_mtime
 
-    def current_on_disk(self):
+    def current_on_disk(self) -> bool:
         return self._mtime == self._disk_mtime
 
 
@@ -240,7 +251,7 @@ class BufferLines:
     #: available.
     lines: List[Optional[str]]
 
-    def __init__(self, buf, textfilter=None, *, cache_debug: bool = False):
+    def __init__(self, buf: GtkSource.Buffer, textfilter: Optional[Any] = None, *, cache_debug: bool = False) -> None:
         self.buf = buf
         if textfilter is not None:
             self.textfilter = textfilter
@@ -269,10 +280,10 @@ class BufferLines:
     def clear_cache(self) -> None:
         self.lines = [None] * self.buf.get_line_count()
 
-    def on_insert_text(self, buf, it, text, textlen):
+    def on_insert_text(self, buf: GtkSource.Buffer, it: Gtk.TextIter, text: str, textlen: int) -> None:
         buf.move_mark(self.mark, it)
 
-    def after_insert_text(self, buf, it, newtext, textlen):
+    def after_insert_text(self, buf: GtkSource.Buffer, it: Gtk.TextIter, newtext: str, textlen: int) -> None:
         start_idx = buf.get_iter_at_mark(self.mark).get_line()
         end_idx = it.get_line() + 1
         # Replace the insertion-point cache line with a list of empty
@@ -281,12 +292,12 @@ class BufferLines:
         # single insertion point line with several empty cache lines.
         self.lines[start_idx:start_idx + 1] = [None] * (end_idx - start_idx)
 
-    def on_delete_range(self, buf, it0, it1):
+    def on_delete_range(self, buf: GtkSource.Buffer, it0: Gtk.TextIter, it1: Gtk.TextIter) -> None:
         start_idx = it0.get_line()
         end_idx = it1.get_line() + 1
         self.lines[start_idx:end_idx] = [None]
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any) -> Any:
         if isinstance(key, slice):
             lo, hi, _ = key.indices(self.buf.get_line_count())
 
@@ -311,26 +322,26 @@ class BufferLines:
 
             return self.lines[key]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.buf.get_line_count()
 
 
 class BufferAction:
     """A helper to undo/redo text insertion/deletion into/from a text buffer"""
 
-    def __init__(self, buf, offset, text):
+    def __init__(self, buf: GtkSource.Buffer, offset: int, text: str) -> None:
         self.buffer = buf
         self.offset = offset
         self.text = text
 
-    def delete(self):
+    def delete(self) -> List[Any]:
         start = self.buffer.get_iter_at_offset(self.offset)
         end = self.buffer.get_iter_at_offset(self.offset + len(self.text))
         self.buffer.delete(start, end)
         self.buffer.place_cursor(end)
         return [self]
 
-    def insert(self):
+    def insert(self) -> List[Any]:
         start = self.buffer.get_iter_at_offset(self.offset)
         self.buffer.place_cursor(start)
         self.buffer.insert(start, self.text)
